6964a9191abf97f6da66845733256f98e05bec65
prathyusha2898 <52311714+prathyusha2898@users.noreply.github.com>
2021-01-13 09:39:45 +0530

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -1,65 +1,45 @@
 
-const mongoose=require('mongoose')
-let mongoXlsx = require('mongo-xlsx');
+var mongoose=require('mongoose')
+// var Schema=mongoose.Schema
 
-var excel = require("exceljs");
-
-const User=mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-     role: {
-  type: String,
-  default: 'basic',
-  enum: ["basic",  "admin"]
- },
-    createdAt:{
-        type:Date,
-      default:Date.now()
-    }
-
+var addressModelSchema = new mongoose.Schema({
+  city: String,
+  street: String,
+  houseNumber: String
 })
+mongoose.model('address',addressModelSchema ,'address' )
 
+// contactInfo model
+var contactInfoModelSchema = new mongoose.Schema({
 
+  email: [String],
+  address: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'address'
+  }
+})
+mongoose.model('contactInfo ',contactInfoModelSchema ,'contactInfo ')
 
-
-let model = mongoXlsx.buildDynamicModel(User);
-mongoXlsx.mongoData2Xlsx(User, model, function (err, res) {
-    if (err) throw err;
-    console.log(res);
-	
-	let workbook = new excel.Workbook(); //creating workbook
-	let worksheet = workbook.addWorksheet('User'); //creating worksheet
-	
-	//  WorkSheet Header
-	worksheet.columns = [
-		{ header: 'Id', key: '_id', width: 10 },
-		{ header: 'Name', key: 'username', width: 30 },
-		{ header: 'email', key: 'email', width: 30},
-		{ header: 'role', key: 'role', width: 10, outlineLevel: 1}
-	];
-	
-	// Add Array Rows
-	// worksheet.addRows(res);
-	
-	// Write to File
-	workbook.xlsx.writeFile("customer.xlsx")
-		.then(function() {
-			console.log("file saved!");
-		});
-	
-   
-  });
-
-
-
-module.exports=mongoose.model("user",User)
+// customer model
+var userSchema = new mongoose.Schema({
+  firstName: {
+    type:String
+  },
+  lastName: {
+    type:String
+  },
+  email:{
+    type:String
+  },
+  password:{
+    type:String
+  },
+  company: String,
+  tel:Number,
+  contactInfo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'contactInfo'
+  }  
+});
+var User=mongoose.model('User', userSchema,)
+module.exports=User
