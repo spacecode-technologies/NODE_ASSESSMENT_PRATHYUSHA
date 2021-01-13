01f9ca4cf1b49636af4a439c37cab35ca4256b8f
prathyusha2898 <52311714+prathyusha2898@users.noreply.github.com>
2021-01-13 09:42:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,97 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const InitiateMongoServer = require("./config/db");
+var express = require("express");
+
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const cors = require("cors");
+
 
 const router = express.Router();
-// Initiate Mongo Server
-InitiateMongoServer();
+app.use("/", router);
 
-const app = express();
-
-// PORT
-const PORT = process.env.PORT || 4000;
-
-// Middleware
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
-});
+const User=require('./Models/User')
+const Company=require('./Models/Company')
+const Exceljs=require('exceljs')
+var connect=require("./db");
 
 
+var { buildSchema } = require('graphql');
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+var root = {
+    message: () => 'Hello World!'
+};
+// Create an express server and a GraphQL endpoint
 
-var authController = require('./authController');
-app.use('/api/auth', authController);
-
-// app.use('/',route)
-
-app.listen(PORT, (req, res) => {
-  console.log(`Server Started at PORT ${PORT}`);
-});
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 
 
-module.exports = app;
+
+
+app.use('/',require('./Controller/pincode_controller'))
+app.use("/users", require("./Routes/user_route"));
+
+
+app.use('*', cors());
+
+
+app.get('/prassi',(req,res)=>{
+  User.find({},(err,res)=>{
+console.log(res)
+  })
+})
+
+
+
+app.get('/sheet',(req,res)=>{
+  
+      User.find({},(err,users)=>{
+        console.log(users)
+        let workbook = new Exceljs.Workbook(); 
+        let worksheet = workbook.addWorksheet('My User');
+        worksheet.columns = [
+          { header: 'Id', key: '_id', width: 10 },
+          { header: 'Name', key: 'firstName', width: 30 },
+          { header: 'Name', key: 'lastName', width: 30 },
+          { header: 'email', key: 'email', width: 30},
+          { header: 'phoneNumber', key: 'tel', width: 30},
+          { header: 'adress', key: 'adress', width: 30},
+          { header: 'company', key: 'company', width: 30},
+        ];
+        console.log(users)
+        users.forEach(user => {
+          worksheet.addRow(user)
+        });
+  
+      const data=  workbook.xlsx.writeFile("customer.xlsx")
+            console.log(data)
+              console.log("file saved!");
+            res.send('done')
+         
+      })
+    
+//creating workbook
+	 //creating worksheet
+    
+  })
+
+
+
+
+app.listen(8000);
+console.log("Listening to PORT 8000");
+
